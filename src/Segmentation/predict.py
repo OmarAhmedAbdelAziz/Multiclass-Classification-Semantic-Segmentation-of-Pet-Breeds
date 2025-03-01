c436683655a85546e6d8c3e067c99db321ca4823
Omar Abd El-Aziz <oabdelaziz@abuerdan.com>
2025-03-01 21:43:43 +0200

    adding src files to segmentation

--- src/Segmentation/predict.py
+++ src/Segmentation/predict.py
@@ -8,21 +8,21 @@ from train import iou_metric, dice_coefficient
 
 def predict_segmentation(image_path, model_path="best_segmentation_checkpoint.h5", img_size=224):
     
-    # Load the model
+    
     custom_objects = {
         'iou_metric': iou_metric,
         'dice_coefficient': dice_coefficient
     }
     model = load_model(model_path, custom_objects=custom_objects)
     
-    # Preprocess the image using the existing function
+    # Preprocess the image 
     img = preprocess_image(image_path, img_size)
     
     # Make prediction
     prediction = model.predict(np.expand_dims(img, axis=0))
     mask = np.argmax(prediction[0], axis=-1)
     
-    # Display results
+    
     plt.figure(figsize=(10, 5))
     plt.subplot(1, 2, 1)
     plt.imshow(img)
--- src/Segmentation/preprocess.py
+++ src/Segmentation/preprocess.py
@@ -11,7 +11,7 @@ def preprocess_image(image_path, img_size=224):
         
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = cv2.resize(img, (img_size, img_size))
-    img = img / 255.0  # Normalize to [0,1]
+    img = img / 255.0  
     
     return img
 
